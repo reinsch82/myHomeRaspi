afbba141154bc9c32c2615be8cf36afbf8f76976
Reinhold Degenfellner <reinhold.jun@degenfellner.com>
2014-09-19 19:26:57 +0200

    implement basic website support

--- http_server.py
+++ http_server.py
@@ -15,7 +15,18 @@ def switchOff(*args):
     print "switch off pins " + str(args)
     for arg in args:
         piFace.output_pins[arg].turn_off();
-    
+
+def switchPins(*args):
+    print "switch pins " + str(args)
+    for pin in args:
+        if pin[1]:
+            print "pin " + str(pin[0]) + " switched on"
+            piFace.output_pins[pin[0]].turn_on()
+        else:
+            print "pin " + str(pin[0]) + " switched off"
+            piFace.output_pins[pin[0]].turn_off()
+    Timer(25.0, switchOff, [0, 1]).start()
+
     
 "SimpleHTTPServer.SimpleHTTPRequestHandler"
 class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
@@ -44,13 +55,10 @@ class RequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         (scm, netloc, path, params, query, fragment) = urlparse.urlparse(self.path, 'http')
         print "path: " + path
         if path.startswith("/down"):
-            piFace.output_pins[0].turn_on()
-            piFace.output_pins[1].turn_on()
-            Timer(25.0, switchOff, [0, 1]).start()
+            switchPins((0, True), (1, True)) 
         if path.startswith("/up"):
-            piFace.output_pins[0].turn_on()
-            piFace.output_pins[1].turn_off()
-            Timer(25.0, switchOff, [0]).start()
+            switchPins((0, True), (1, False))
+        self.send_response(200)
 
 
             
